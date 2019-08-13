c21433e873903693accb471324f4ab273648b593
Xinyi Wu <xxinyiwho@gmail.com>
2019-08-13 11:53:59 +0200

    created superpowers controller

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -1,7 +1,50 @@
 class SuperpowersController < ApplicationController
-  def show
-  end
+
+  before_action :find
 
   def index
+    @superpowers = Superpower.all
   end
+
+  def show
+    # find method
+  end
+
+  def new
+    @superpower = Superpower.new
+  end
+
+  def create
+    @superpower = Superpower.new(superpower_strong_params)
+    if @superpower.save
+      redirect_to superpower_path(@superpower)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    # find method
+  end
+
+  def update
+    # find method
+    @superpower.update(superpower_strong_params)
+    redirect_to superpower_path(@superpower)
+  end
+
+  def destroy
+    # find method
+    @superpower.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def find
+    @superpower = Superpower.find(params[:id])
+  end
+
+  def superpower_strong_params
+    params.require(:superpowers).permit(:name, :description, :price, :photo, :address)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # get 'superpowers/show'
   # get 'superpowers/index'
   root to: 'pages#home'
-resources :superpowers, only: [ :index, :new, :create, :show, :destroy] do
-    resources :bookings, only: [:new, :create]
-
-
+  resources :superpowers do
+  resources :bookings, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
