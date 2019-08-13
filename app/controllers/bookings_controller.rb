a137d9d6d91b4da9e18d95089745e446506391e2
Josephine Geoghegan <josephineanngeoghegan@gmail.com>
2019-08-13 11:52:01 +0200

    controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,29 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def create
+    @booking = Booking.new(booking_strong_params)
+    if @booking.save
+      redirect_to
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_strong_params
+    params.require(:booking).permit (:start_date, :end_date, :start_date, :status)
+  end
+
 end
