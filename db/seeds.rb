96e2b9a8845d85f33187e08a2ba5331bc11b2d52
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2019-08-13 17:28:16 +0200

    index frontend part 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,57 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-user = User.create(email: "xinyi@gmail.com", password: "password")
-fly = Superpower.create(name: "Fly", description: "Fly me to the sky")
-fly.user = user
+
+puts "starting seed"
+
+Superpower.destroy_all
+User.destroy_all
+puts "destroying old seeds ha ha ha"
+
+user = User.create!(email: "xinyi@gmail.com", password: "password")
+
+Superpower.create!(
+  name: "Fly",
+  description: "Fly me to the sky",
+  user: user
+  )
+
+Superpower.create!(
+  name: "Throwing ball of fire",
+  description: "Fire is hot baby",
+  user: user
+  )
+
+Superpower.create!(
+  name: "Ice",
+  description: "Cold as your grandma",
+  user: user
+  )
+
+Superpower.create!(
+  name: "Invisibility",
+  description: "Go everywhere without being found out",
+  user: user
+  )
+
+Superpower.create!(
+  name: "Superstrenght",
+  description: "Carry your car home!",
+  user: user
+  )
+
+Superpower.create!(
+  name: "Speak to animals",
+  description: "Speak with your best buddy!",
+  user: user
+  )
+
+Superpower.create!(
+  name: "Teleporting",
+  description: "Going to school in 1 second is tight!",
+  user: user
+  )
+
+
+puts "Done"
+
