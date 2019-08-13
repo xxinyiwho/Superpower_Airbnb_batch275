39f20b8092c54caea4c041abaaa40938c2454cba
Maria Manninen <maria.vihla@gmail.com>
2019-08-13 12:32:58 +0200

    finishing devise

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   # get 'superpowers/show'
   # get 'superpowers/index'
   root to: 'pages#home'
-resources :superpowers, only: [ :index, :new, :create, :show, :destroy] do
+
+  resources :superpowers, only: [ :index, :new, :create, :show, :destroy] do
     resources :bookings, only: [:new, :create]
+  end
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
