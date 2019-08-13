4850691e9b822b8fe576c71d825933f330568103
Maria Manninen <maria.vihla@gmail.com>
2019-08-13 11:32:04 +0200

    recreated model user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'superpowers/show'
-  get 'superpowers/index'
+  # get 'superpowers/show'
+  # get 'superpowers/index'
   root to: 'pages#home'
+resources :superpowers, only: [ :index, :new, :create, :show, :destroy] do
+    resources :bookings, only: [:new, :create]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
