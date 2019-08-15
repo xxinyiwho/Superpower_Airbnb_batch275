41774d2ccc27031171081ce8f969ba8bc3a0b09e
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2019-08-15 17:20:06 +0200

    dashboard issues

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,8 @@
 class DashboardsController < ApplicationController
 
    def show
-    @superpowers = Superpower.where(user: current_user)
-    @bookings = Booking.where(user: current_user)
+    @superpowers = Superpower.where(user: current_user) if current_user.superpowers.any?
+    @bookings = Booking.where(user: current_user) if current_user.bookings.any?
   end
 
 end
