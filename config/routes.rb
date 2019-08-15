2f19ad4c15099a4b506fc0b74dda132c0b491aeb
Maria Manninen <maria.vihla@gmail.com>
2019-08-15 13:17:10 +0200

    Booking form on showpage fixed and ugly booking showpage created

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :superpowers do
     resources :bookings, only: [:new, :create]
   end
+  resources :bookings, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
