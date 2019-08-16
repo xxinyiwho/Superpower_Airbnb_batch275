ba38156c4ab284ed4f113f87af88c3b0e3dc21e7
Thomas Bonnet <thomas.bonnet.12@gmail.com>
2019-08-16 15:54:03 +0200

    erase and update buttons added to dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,9 @@ Rails.application.routes.draw do
   resources :superpowers do
     resources :bookings, only: [:new, :create, :edit, :update]
   end
-  resources :bookings, only: [:show]
+  resources :bookings, only: [:show, :destroy]
 
-  get "/dashboard", to: "dashboards#show"
+  get "/dashboard", to: "dashboards#show", as: "dashboard"
   patch "/bookings/:id", to: "bookings#accept_booking", as: "/accept"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
